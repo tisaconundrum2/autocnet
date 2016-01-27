c820761a8f9578b6d7c89837882c627da871c5ba
kberry <kberry@usgs.gov>
2016-01-27 13:31:28 -0700

    New branch subpixel, merged with upstream

---
+++ autocnet/matcher/tests/test_subpixel.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.abspath('..'))
+
+from .. import subpixel
+
+class TestSubpixel(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_subpixel(self):
+        self.assertTrue(False)
+
+    def tearDown(self):
+        pass
