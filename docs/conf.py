27d63fe95d54b066d8dedadcd14deabcebcc09ea
jlaura <jlaura@asu.edu>
2016-03-10 15:07:21 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
 MOCK_MODULES = ['proj4', 'gdal', 'osr','ogr', 'cv2', 'scikit-image', 'skimage', 'skimage.feature', 'sklearn',
                 'scipy', 'scipy.spatial', 'scipy.spatial.distance', 'scipy.ndimage', 'scipy.ndimage.interpolation',
                 'scipy.constants', 'scipy.stats','scipy.cluster.vq', 'scipy.sparse', 'scipy.stats.mstats', 'scipy.special',
-                'scipy.optimize', 'osgeo']
+                'scipy.optimize', 'scipy.stats.stats', 'osgeo']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
