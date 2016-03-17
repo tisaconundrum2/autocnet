7673a2fd87bb8956cb3a889bd22dc108dc8f1dcb
Jay <jlaura@asu.edu>
2016-03-17 15:23:55 -0700

    HDF5 datastore in place - need to patch in to the node now

---
+++ autocnet/fileio/tests/test_io_hdf.py
@@ -0,0 +1,15 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+
+from .. import io_hdf
+
+
+class TestHDF(unittest.TestCase):
+
+    def test_df_sarray(self):
+        self.assertTrue(False)
+
+    def test_sarray_df(self):
+        self.assertTrue(False)
