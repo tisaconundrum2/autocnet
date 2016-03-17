48df31151a2b507e8a0a119b0879f06732e871e8
Adam Paquette <acpaquette@nacho.wr.usgs.gov>
2016-03-17 09:49:48 -0700

    Premtive pep8 clean up

--- bin/image_match.py
+++ bin/image_match.py
@@ -1,18 +1,16 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../autocnet/'))
 
-from autocnet.examples import get_path
 from autocnet.graph.network import CandidateGraph
-from autocnet.matcher.matcher import FlannMatcher
 from autocnet.fileio.io_controlnetwork import to_isis
 from autocnet.fileio.io_controlnetwork import write_filelist
 
-from IPython.display import display
+sys.path.insert(0, os.path.abspath('../autocnet/'))
+
 cg = CandidateGraph.from_adjacency(sys.argv[1], basepath='/home/acpaquette/Desktop/')
 
 # Apply SIFT to extract features
-cg.extract_features(method='sift', extractor_parameters={'nfeatures':500})
+cg.extract_features(method='sift', extractor_parameters={'nfeatures': 500})
 
 # Match
 cg.match_features()
@@ -26,12 +24,11 @@ m = cg.edge[0][1].masks
 # Compute a homography and apply RANSAC
 cg.compute_fundamental_matrices(clean_keys=['ratio', 'symmetry'])
 
-cg.subpixel_register(clean_keys=['fundamental', 'symmetry', 'ratio'],template_size=5, search_size=15)
+cg.subpixel_register(clean_keys=['fundamental', 'symmetry', 'ratio'], template_size=5, search_size=15)
 
-cnet = cg.to_cnet(clean_keys = ['subpixel'], isis_serials=True)
+cnet = cg.to_cnet(clean_keys=['subpixel'], isis_serials=True)
 
 filelist = cg.to_filelist()
 write_filelist(filelist, 'TestList.lis')
 
-to_isis('TestList.net', cnet, mode = 'wb',targetname='Moon')
-
+to_isis('TestList.net', cnet, mode='wb', targetname='Moon')
