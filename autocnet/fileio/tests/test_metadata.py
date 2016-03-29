3b638ec6bac1ac5dfa2314987487f3d5342fa8b6
Jay <jlaura@asu.edu>
2016-03-28 19:31:18 -0700

    Fixes for GDAL 2.0.2 upgrade - looks like proj4 string formatting changes

--- autocnet/fileio/tests/test_metadata.py
+++ autocnet/fileio/tests/test_metadata.py
@@ -24,7 +24,7 @@ class TestSRSProjectionExtraction(unittest.TestCase):
 
     def test_projection_name(self):
         name = em.get_projection_name(self.srs)
-        self.assertEqual(name, 'Mercator_1SP')
+        self.assertEqual(name, 'Mercator_2SP')
 
     def test_axes_extract(self):
         smajor, sminor, invflattening = em.get_spheroid(self.srs)
@@ -46,7 +46,7 @@ class TestSRSProjectionExtraction(unittest.TestCase):
         Check that proj4 is not supporting Moon2000_Mercator
         """
         proj4 = self.srs.ExportToProj4()
-        self.assertEqual(proj4, '+proj=merc +lon_0=180 +k=1 +x_0=0 +y_0=0 +a=1737400 +b=1737400 +units=m +no_defs ')
+        self.assertEqual(proj4, '+proj=merc +lon_0=180 +lat_ts=0 +x_0=0 +y_0=0 +a=1737400 +b=1737400 +units=m +no_defs ')
 
     def test_scale_factor(self):
         k = em.get_scale_factor(self.srs)
@@ -54,4 +54,4 @@ class TestSRSProjectionExtraction(unittest.TestCase):
 
     def test_latitude_of_origin(self):
         lo = em.get_latitude_of_origin(self.srs)
-        self.assertEqual(lo, 0.0)
+        self.assertEqual(lo, 90.0)
