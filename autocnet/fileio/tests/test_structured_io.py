e78e0b367b34100a2a7a77de62a43aedcf7f389e
Jay <jlaura@asu.edu>
2016-04-06 05:26:16 -0700

    More agile conditional import for testing.

--- autocnet/fileio/tests/test_structured_io.py
+++ autocnet/fileio/tests/test_structured_io.py
@@ -3,11 +3,18 @@ import unittest
 from .. import io_yaml
 from .. import io_json
 
+try:
+    import yaml
+    missing = False
+except ImportError:
+    missing = True
+
 from autocnet.examples import get_path
 
 
 class TestYAML(unittest.TestCase):
 
+    @unittest.skipif(missing == True)
     def test_read(self):
         d = io_yaml.read_yaml(get_path('logging.yaml'))
         self.assertIn('handlers', d.keys())
